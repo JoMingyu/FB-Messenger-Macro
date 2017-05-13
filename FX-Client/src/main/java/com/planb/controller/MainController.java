d5c124a1fa2da6ec165f7ecd34538d1e08ded903
JoMingyu <city7310@naver.com>
2017-05-13 23:48:38 +0900

    이벤트 처리

--- FX-Client/src/main/java/com/planb/controller/MainController.java
+++ FX-Client/src/main/java/com/planb/controller/MainController.java
@@ -3,6 +3,7 @@ package com.planb.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -10,6 +11,8 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 public class MainController implements Initializable {
 	@FXML
@@ -25,18 +28,50 @@ public class MainController implements Initializable {
 	private Label infoLabel;
 	
 	@FXML
-	private CheckBox keepLogin;
+	private CheckBox keepLoginBox;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		
 	}
 	
-	public void keyPressedOnField() {
-		
+	public void keyPressedOnField(KeyEvent key) {
+		if(key.getCode() == KeyCode.ENTER) {
+			// Enter -> Login button event
+			loginButtonOnAction();	
+		} else if(key.getCode() == KeyCode.TAB) {
+			// Tab -> switch other text field
+			if(idField.isFocused()) {
+				Platform.runLater(new Runnable () {
+					@Override
+					public void run() {
+						pwField.requestFocus();
+					}
+				});
+			} else if(pwField.isFocused()) {
+				Platform.runLater(new Runnable () {
+					@Override
+					public void run() {
+						idField.requestFocus();
+					}
+				});
+			}
+		}
 	}
 	
 	public void loginButtonOnAction() {
+		String id = idField.getText();
+		String pw = pwField.getText();
+		boolean keepLogin = keepLoginBox.isSelected();
 		
+		if(id.isEmpty()) {
+			infoLabel.setText("Insert ID");
+			return;
+		} else if(pw.isEmpty()) {
+			infoLabel.setText("Insert Password");
+			return;
+		}
+		
+		infoLabel.setText("Waiting");
 	}
 }
