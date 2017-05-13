7e39e16e799c4df952c9f9a57e89ab0bbb297977
JoMingyu <city7310@naver.com>
2017-05-13 22:58:31 +0900

    기본 앱 구성

--- FX-Client/src/main/java/com/planb/main/Main.java
+++ FX-Client/src/main/java/com/planb/main/Main.java
@@ -1,7 +1,30 @@
 package com.planb.main;
 
-public class Main {
-	public static void main(String[] args){
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("/com/planb/layout/layout_main.fxml"));
 		
+		Scene scene = new Scene(root);
+		// root에 대해 scene을 생성
+		
+		primaryStage.setScene(scene);
+		// stage에 scene 설정
+		
+		primaryStage.setTitle("페이스북 메신저 매크로");
+//		primaryStage.getIcons().add(new Image("/com/watchover/layout/xxx.png"));
+		// 타이틀과 아이콘 설정
+		
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
 	}
 }
